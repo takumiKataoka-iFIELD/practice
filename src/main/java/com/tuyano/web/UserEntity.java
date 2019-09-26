6012dea4e3eeab6e12b11a43ddbe10416f28fad9
takumiKataoka <gotty071098g@icloud.com>
2019-09-26 13:38:22 +0900

    edit entity

--- src/main/java/com/tuyano/web/UserEntity.java
+++ src/main/java/com/tuyano/web/UserEntity.java
@@ -1,5 +1,55 @@
 package com.tuyano.web;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class UserEntity {
-bf
+
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+
+	public UserEntity() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
