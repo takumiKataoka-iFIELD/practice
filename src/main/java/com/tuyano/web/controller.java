c33bae3a844cde65df17b3aeeea473bc3884fa0a
tanig <tanig@192.168.11.11>
2019-09-26 13:49:28 +0900

    ありがとうございました。

--- src/main/java/com/tuyano/web/controller.java
+++ src/main/java/com/tuyano/web/controller.java
@@ -1,5 +1,34 @@
 package com.tuyano.web;
 
-public class controller {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class controller {
+	
+	@Autowired
+	userEntity userEntity;
+ 
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String go() {
+		return "";
+	}
+	
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	public String userAdd(@RequestParam(user_name)String user_name,
+							@RequestParam(email)String email,
+							@RequestParam(password)String password,Model model) {
+		userEntity.save(user_name, email, password);
+		return "";
+	}
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String userData(Model model) {
+		return "";
+	}
+	
 }
