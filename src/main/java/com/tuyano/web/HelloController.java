56bf883663fba8fbe38bc5eaeb08d84560574d9c
info <info@192.168.11.6>
2019-09-30 11:35:19 +0900

    @PathValiable

---
+++ src/main/java/com/tuyano/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.tuyano.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num ,Model model) {
+		int res = 0;
+		for(int i=1;i <= num;i++) {
+			res += i;
+		}
+		model.addAttribute("msg","total: " + res);
+		return "book";
+
+	}
+}
