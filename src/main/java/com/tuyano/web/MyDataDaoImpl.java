0bfcc99c799ae9ba240b0933bba5c5bc0ccdf050
info <info@192.168.11.6>
2019-10-01 14:38:53 +0900

    edit MyDataDaoImple

--- src/main/java/com/tuyano/web/MyDataDaoImpl.java
+++ src/main/java/com/tuyano/web/MyDataDaoImpl.java
@@ -27,4 +27,16 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
 		return list;
 	}
 
+	@Override
+	public MyData findById(long id) {
+		return (MyData)entityManager.createQuery("from MyData where id = " + id).getSingleResult();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<MyData> findByName(String name) {
+		return (List<MyData>)entityManager.createQuery("from MyData where name = " + name).getResultList();
+	}
+
 }
