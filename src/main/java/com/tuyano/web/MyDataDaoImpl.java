0ea77fba1a6ceaa8fb1fa4957a6114867e9a60a5
info <info@192.168.11.6>
2019-10-01 11:59:56 +0900

    create daoclass

---
+++ src/main/java/com/tuyano/web/MyDataDaoImpl.java
@@ -0,0 +1,13 @@
+package com.tuyano.web;
+
+import java.util.List;
+
+public class MyDataDaoImpl implements MyDataDao<MyData> {
+
+	@Override
+	public List<MyData> getAll() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
