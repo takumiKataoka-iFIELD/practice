aef48104dcd3b8a20a18d56666769054e2739cdf
info <info@192.168.11.6>
2019-10-01 15:55:46 +0900

    edit MyDataDao

--- src/main/java/com/tuyano/web/MyDataDao.java
+++ src/main/java/com/tuyano/web/MyDataDao.java
@@ -8,4 +8,5 @@ public interface MyDataDao <T> extends Serializable {
 	public List <T> getAll();
 	public T findById(long id);
 	public List <T> findByName(String name);
+	public List <T> find(String fstr);
 }
