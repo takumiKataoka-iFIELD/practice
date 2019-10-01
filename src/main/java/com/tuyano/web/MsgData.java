cca770310561f733f1d0991458e505debf444175
info <info@192.168.11.6>
2019-10-01 16:42:10 +0900

    add MsgData

--- src/main/java/com/tuyano/web/MsgData.java
+++ src/main/java/com/tuyano/web/MsgData.java
@@ -1,5 +1,68 @@
 package com.tuyano.web;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "mdgdata")
 public class MsgData {
+
+	@Id
+	@GeneratedValue
+	@Column
+	private Long id;
+
+	@Column
+	private String title;
+
+	@Column(nullable = false)
+	@NotEmpty
+	private String message;
+
+	@ManyToOne
+	private MyData mydata;
+
+	public MsgData() {
+		mydata = new MyData();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public MyData getMyData() {
+		return mydata;
+	}
+
+	public void setMyData(MyData mydata) {
+		this.mydata = mydata;
+	}
+
+
 
 }
