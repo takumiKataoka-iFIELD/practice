ddc1c2be6fa45b689b81416d0eb50319260fa85c
info <info@192.168.11.6>
2019-10-01 12:15:11 +0900

    edit MyDataDaoImpl

--- src/main/java/com/tuyano/web/MyDataDaoImpl.java
+++ src/main/java/com/tuyano/web/MyDataDaoImpl.java
@@ -2,12 +2,29 @@ package com.tuyano.web;
 
 import java.util.List;
 
-public class MyDataDaoImpl implements MyDataDao<MyData> {
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
+public class MyDataDaoImpl implements MyDataDao<MyData> {
+	private static final long serialVersionUID = 1L;
+
+	private EntityManager entityManager;
+
+	public MyDataDaoImpl() {
+		super();
+	}
+
+	public MyDataDaoImpl(EntityManager manager) {
+		this();
+		entityManager = manager;
+	}
 	@Override
 	public List<MyData> getAll() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		Query query = entityManager.createQuery("from MyData");
+		@SuppressWarnings("unchecked")
+		List <MyData> list = query.getResultList();
+		entityManager.close();
+		return list;
 	}
 
 }
