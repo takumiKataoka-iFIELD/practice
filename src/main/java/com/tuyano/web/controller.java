5ec3828a02d962c2618e78c40cfb5bb6a40734de
tanig <tanig@192.168.11.11>
2019-10-02 15:01:45 +0900

    ouho

--- src/main/java/com/tuyano/web/controller.java
+++ src/main/java/com/tuyano/web/controller.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.support.SessionStatus;
 @Controller
 @SessionAttributes("logName")
 public class controller {
-	
+	ljl
 	
 	@Autowired
 	UserEntityRepository UserEntityRepository;
