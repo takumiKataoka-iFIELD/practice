5efb8eba8b903bfb7949b3849e64c1fbd96fac2a
info <info@192.168.11.6>
2019-10-02 12:25:58 +0900

    edit MsgController

--- src/main/java/com/tuyano/web/MsgController.java
+++ src/main/java/com/tuyano/web/MsgController.java
@@ -1,5 +1,35 @@
 package com.tuyano.web;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class MsgController {
 
+	@Autowired
+	MsgDataRepository repository;
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	MsgDataDaoImpl dao;
+
+	@RequestMapping("/msg")
+	public ModelAndView msg(ModelAndView mav) {
+		mav.setViewName("showMsgData");
+		mav.addObject("title","Sample");
+		mav.addObject("msg","MsgDataのサンプルです。");
+		MsgData msgdata = new MsgData();
+		mav.addObject("formModel",msgdata);
+		List<MsgData> list = dao.getAll();
+		mav.addObject("data",list);
+		return mav;
+	}
 }
